0ff1f692820f4106aac57c82904addb46037b1f1
Ben Asher <basher@mobiata.com>
2012-06-10 19:27:45 -0700

    updated readme and bumped version number

--- init.rb
+++ init.rb
@@ -12,8 +12,8 @@ Redmine::Plugin.register :redmine_default_version do
   name 'Redmine Default Version plugin'
   author 'Tony Marschall'
   description 'With this plugin its possible to add a default version to a project. First you have to setup a version then you can choose a default version in project -> settings.'
-  version '0.0.1'
-  url ' https://github.com/tonymarschall/redmine_default_version/'
+  version '0.0.2'
+  url ' https://github.com/benasher44/redmine_default_version/'
 end
 
 require_dependency 'redmine_default_version/hooks/view_projects_form_hook'
